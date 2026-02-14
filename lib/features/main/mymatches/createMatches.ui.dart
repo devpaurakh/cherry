31ec42cf453752dded5cf38b037bc67632bf65b5
Paurakh Saud <paurakhsaud2022@gmail.com>
2026-02-14 22:19:46 +0545

    (Added) new file for create meatches

---
+++ lib/features/main/mymatches/createMatches.ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateMatches extends StatefulWidget {
+  const CreateMatches({super.key});
+
+  @override
+  State<CreateMatches> createState() => _CreateMatchesState();
+}
+
+class _CreateMatchesState extends State<CreateMatches> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
