62a77590cc102bf8e013e86630a2a8d49feb4b75
Paurakh Saud <paurakhsaud2022@gmail.com>
2026-02-19 17:14:08 +0545

    (Added)file to the project

---
+++ lib/features/main/MatchDetail/batting.ui.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Batting extends StatefulWidget {
+  const Batting({super.key});
+
+  @override
+  State<Batting> createState() => _BattingState();
+}
+
+class _BattingState extends State<Batting> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 16, vertical: 10),
+        child: Column(children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
