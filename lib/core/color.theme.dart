32d32aa311f3449a08f76105505fc8e7f9e80b00
Paurakh Saud <paurakhsaud2022@gmail.com>
2026-02-21 15:10:12 +0545

    (Added) new 3 colors

--- lib/core/color.theme.dart
+++ lib/core/color.theme.dart
@@ -7,10 +7,19 @@ class AppColors {
   static const Color backgroundColor = Color(0xFFF4F6FB);
   static const Color secondaryColor = Color(0xFFFFE6C9);
   static const Color warning = Color(0xFFE5533D);
+  static const Color error = Color(0xFFFF0000);
   static const Color success = Color(0xFF008000);
   static const Color containerColor = Color(0xFFE8EEF7);
   static const Color titleTextColor = Color(0xFF162447);
   static const Color bodyTextColor = Color(0xFF4A5568);
   static const Color black = Color(0xFF000000);
   static const Color white = Color(0xFFFFFFFF);
+
+  // const Color(0xFF009688), // Teal
+  // const Color(0xFFFFC107), // Amber
+  // const Color(0xFF795548), // Brown
+
+  static const Color teal = Color(0xFF009688);
+  static const Color amber = Color(0xFFFFC107);
+  static const Color brown = Color(0xFF795548);
 }
